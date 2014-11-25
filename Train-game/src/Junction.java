42d6afefb07f96c78935f4d432b77cdeda6cc1d8
R Brierley <rb1038@pc973s.cs.york.ac.uk>
2014-11-25 14:31:35 +0000

    Added classes and attributes

--- Train-game/src/Junction.java
+++ Train-game/src/Junction.java
@@ -1,1 +1,34 @@
-//This is a test
+
+public class Junction 
+{
+	int JunctionID;
+	int TracksConnectedList[];
+	int TrainsPresent[];
+
+	public static void main(String[] args) 
+	{
+		// This is a comment
+		// TODO Auto-generated method stub
+
+	}
+	
+	private static int AddTrain()
+	{
+		
+		return 0;
+	}
+	
+	private static int RemoveTrain()
+	{
+		
+		return 0;
+	}
+
+	private static int GetJunctionID()
+	{
+		
+		return 0;
+	}
+	
+	private static void SetJunctionID()
+	{
--- Train-game/src/MapGraph.java
+++ Train-game/src/MapGraph.java
@@ -1,11 +1,34 @@
 
-public class MapGraph {
-	int TurnCounter;
-	int CurrentPlayer;
+public class Junction 
+{
+	int JunctionID;
+	int TracksConnectedList[];
+	int TrainsPresent[];
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		// This is a comment
 		// TODO Auto-generated method stub
 
 	}
+	
+	private static int AddTrain()
+	{
+		
+		return 0;
+	}
+	
+	private static int RemoveTrain()
+	{
+		
+		return 0;
+	}
 
-}
+	private static int GetJunctionID()
+	{
+		
+		return 0;
+	}
+	
+	private static void SetJunctionID()
+	{
