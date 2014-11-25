138725e63b8ad33339657b681fa3711a7872baf1
em948 <em948@york.ac.uk>
2014-11-25 21:10:43 +0000

    PseudoCode written in Java using a different approach. Used only methods available in UML. Didn't need to add Station.TrainList....
    <CurrentTurn (Global) needs be returned by Game or Map>

--- Train-game/src/Goal.java
+++ Train-game/src/Goal.java
@@ -7,6 +7,7 @@ public class Goal
 	Junction StartLoc;
 	Junction DestLoc;
 	ArrayList<int[]> GoalStarted;
+	ArrayList<Train> TrainList;
 	int TurnLimit;
 	int NoCarriages;
 	int GoalID;
@@ -14,16 +15,20 @@ public class Goal
 	public Goal (int GoalID, Junction StartLoc, Junction DestLoc, int TurnLimit, int NoCarriages)
 	{
 		this.GoalID = GoalID;
-		this.StartLoc = StartLoc;
+		this.StartLoc = StartLoc; //SET TO -1 IF NO STARTLOC
 		this.DestLoc = DestLoc;
-		this.TurnLimit = TurnLimit; //SET TO 0 IF NO LIMIT
+		this.TurnLimit = TurnLimit; //SET TO -1 IF NO LIMIT
 		this.NoCarriages = NoCarriages;
 		this.GoalStarted = new ArrayList<int[]>();
+		this.TrainList = new ArrayList<Train>();
 	}
 	
-	public void GoalStartedForTrain(int TurnCount, int TrainID){
-		int[] pair = new int[]{TurnCount, TrainID};
-		this.GoalStarted.add(pair);
+	public void GoalStartedForTrainX(int TurnCount, Train TrainX)
+	{
+		this.TrainList.add(TrainX);
+		int flag1 = 0;
+		int[] triple = new int[]{TrainX.TrainID, TurnCount, flag1};
+		this.GoalStarted.add(triple);
 	}
 	
 	public int GetID()
@@ -31,40 +36,76 @@ public class Goal
 		return this.GoalID;
 	}
 	
-	public boolean CheckComplete(int CurrentTurnCount, int TrainID, ArrayList<?> TrainsAtDestStation)
+	public int TrainBeenToStart(Train TrainX) 
 	{
-		boolean CompleteFlag = true; //Uses a flag to determine whether the goal is complete, assume true- determine otherwise
-		
-		//IF THERE WAS A START STATION...
-			//IF TRAIN HAS BEEN TO START STATION...
-				//IF THERE WAS A TURN LIMIT...	
-					CompleteFlag = CompleteFlag && TurnLimitExceeded(CurrentTurnCount, TrainID);
-				//ELSE TRAIN HAS BEEN TO START STATION- IF IT IS AT DEST STATION-> GOAL COMPLETE
-					//Return True?
-				//ENDIF
-			//ELSE TRAIN NOT BEEN TO START
-				//Return False?
-			//ENDIF
-		//NO START STATION
-			//
-		return CompleteFlag; //TODO THIS METHOD STILL NEEDS IMPLEMENTING
-	}
-	
-	private boolean TurnLimitExceeded(int CurrentTurnCount, int TrainID)
-	{
-		
-		/*for(int i = 0; i < this.GoalStarted.size(); i++)
+		for (int i, i < this.GoalStarted.size(), i++ )
 		{
-			int TurnCountWhenStarted = this.GoalStarted.get(i)[0];
-			if(this.TurnLimit <= (CurrentTurnCount - TurnCountWhenStarted)){
-				//Turn Limit has not been exceeded
-				return false;
-			}
-			else{
-				//Turn Limit has been exceeded for this train
-				this.GoalStarted.remove(i);
+			if (this.GoalStarted.get(i)[0] == TrainX.TrainID)
+			{
+				if (TrainX.CurrentJunction == this.Startloc)
+				{
+					this.GoalStarted.get(i)[2] = 1				
+				}					
 			}
 		}
-		return false;*/
+		return this.GoalStarted.get(i)[2];
 	}
+			
+	public boolean CheckComplete(int CurrentTurnCount, Train TrainX) // ArrayList<?> TrainsAtDestStation
+	//can display more info if needed, than just False, if needed <Train has not yet reached starting place, Train has not attempted this goal, turn limit exceeded>
+	{
+		if (this.TrainList.contains(TrainX)== false)
+		{
+			return false; 
+			//TrainX Object Has Not Taken This Goal
+		}
+		
+		if (this.TurnLimitExceeded) //Train has exceeded Turn limit
+		{
+			return false 
+			//and goal deleted 
+		}
+		
+		if (this.StartLoc != -1) //If a start location is provided 
+		{
+			if (this.TrainBeenToStart(TrainX) == 0) //train has not yet been to start location => flag1 = 0
+			{
+				return false 
+						//train hasn't yet completed the goal
+			}
+		}
+		//train has already been to start location or the start location is not provided & is now at final destination
+		if (TrainX.CurrentJunction== this.DestLoc) 
+		{
+			return true 
+		}		
+	}
+	
+	private boolean TurnLimitExceeded(int CurrentTurnCount, Train TrainX) 
+	//Combining case of if Turn limit is not privded in this. If needed, can create separate method.
+	{
+		if (this.TurnLimit = -1)
+		{
+			return False; 
+			//TurnLimit Wasn't Provided So Can't Be Exceeded for any of the trains
+		}
+		
+		for (int i = 0, i < this.GoalStarted.size(), i++ )
+		{
+			int TurnCountWhenStarted = this.GoalStarted.get(i)[1];
+			
+			if ( (CurrentTurnCount - TurnCountWhenStarted) <= this.TurnLimit)
+			{
+				return false; 
+				//Turn Limit has not been exceeded for this train
+			}
+			else			
+			{
+			this.GoalStarted.remove(i);	
+			return True;
+			}
+		}
 }
+
+//	boolean CompleteFlag = true ; //Uses a flag to determine whether the goal is complete, assume true- determine otherwise
+
