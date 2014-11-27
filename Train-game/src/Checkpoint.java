47e497a9d3b1dc814049186c713f4c02475f4658
sw1308 <sw1308@york.ac.uk>
2014-11-27 13:47:24 +0000

    Added some mutator methods to Checkpoint

--- Train-game/src/Checkpoint.java
+++ Train-game/src/Checkpoint.java
@@ -3,6 +3,7 @@ package MapGraph;
 public class Checkpoint extends Junction
 {
 	bool IsFaulty;
+	String FaultMessage;
 	
 	public Checkpoint(int ID)
 	{
@@ -12,6 +13,18 @@ public class Checkpoint extends Junction
 	public Checkpoint(int ID, int[][] JunctionsConnected, int[] TrainsPresent)
 	{
 		this.IsFaulty = false;
+		this.FaultMessage = "";
 		super(ID, JunctionsConnected, TrainsPresent);
+	}
+	
+	protected void RepairFault()
+	{
+		this.IsFaulty = false;
+	}
+	
+	protected void CauseFault(String Message)
+	{
+		this.IsFaulty = true;
+		this.FaultMessage = Message;
 	}
 }
